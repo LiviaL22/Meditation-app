4046227f200f30840aa1340a4c875b63da6ffd06
Livia <livialuciani93@gmail.com>
2023-11-08 12:48:29 +0100

    add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import style from './Footer.module.css';
 export default function Footer () {
     return (
         <>
-        <div className={style.footer}>
+        <footer className={style.footer}>
          <p className={style.copy}>Livia Luciani &copy; 2023</p>
-        </div>
+        </footer>
         </>
     )
 }
