4f7d9120f0b440a18de538f947d9649e85c0742f
Livia <livialuciani93@gmail.com>
2024-03-14 19:51:51 +0100

    mod

--- src/components/Sound/Sound.jsx
+++ src/components/Sound/Sound.jsx
@@ -1,29 +1,18 @@
-import style from './Sound.module.css';
-import img from '../../assets/image/relax.jpg';
-import sound from '../../assets/sound/please-calm-my-mind.mp3'
+import style from "./Sound.module.css";
+import img from "../../assets/image/relax.jpg";
+import sound from "../../assets/sound/please-calm-my-mind.mp3";
 
 export default function Sound() {
- 
- let audio = new Audio(sound);
- const start = () => {
-    if (audio.paused === true) {
-        audio.play();
-    } else {
-        audio.pause();
-  audio.currentTime = 0;
-        audio.stop();
-        
-    }
-};
+  let audio = new Audio(sound);
 
- return(
+  const start = () => {
+    audio.paused ? audio.play() : audio.pause();
+  };
 
+  return (
     <button className={style.button} onClick={start}>
       <img className={style.img} src={img} alt="image/soundpage" />
-      <audio loop src={sound}/>
+      <audio src={sound} />
     </button>
-   
-    );    
-     
-  
+  );
 }
--- src/pages/Timer/Timer.jsx
+++ src/pages/Timer/Timer.jsx
@@ -1,148 +1,130 @@
-import style from './Timer.module.css';
-import Sound from '../../components/Sound/Sound';
-import { useEffect, useState } from 'react';
-import {IoMdMusicalNote} from 'react-icons/io';
-
+import style from "./Timer.module.css";
+import Sound from "../../components/Sound/Sound";
+import { useEffect, useState } from "react";
+import { IoMdMusicalNote } from "react-icons/io";
 
 export default function Timer() {
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState("0" + 0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isStopped, setIsStopped] = useState(false);
 
-    const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState("0" + 0);       
-    const [isRunning, setIsRunning] = useState(false);
-    const [isStopped, setIsStopped] = useState(false);    
-    
-
-    useEffect(() => {
-        let interval;
-		if (isRunning) {
-            interval = setTimeout(() => {
-                if (seconds > 0 && seconds < 11) {                        
-                    setSeconds((prevSeconds) => "0" + (prevSeconds - 1));
-                } else if (seconds >= 11){
-                    setSeconds((prevSeconds) => (prevSeconds - 1));
-                } else if (minutes > 0) {
-                    setMinutes(prevMinutes => prevMinutes - 1);
-                    setSeconds(59);
-                }
-            }, 1000)
-		} 
-
-        if (minutes === 0 && seconds === 0) {
-            setIsRunning(false);
-            setSeconds(0);
-            setMinutes(0);
-            setIsStopped(true);
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setTimeout(() => {
+        if (seconds > 0 && seconds < 11) {
+          setSeconds((prevSeconds) => "0" + (prevSeconds - 1));
+        } else if (seconds >= 11) {
+          setSeconds((prevSeconds) => prevSeconds - 1);
+        } else if (minutes > 0) {
+          setMinutes((prevMinutes) => prevMinutes - 1);
+          setSeconds(59);
         }
-
-        return () => clearTimeout(interval);
-	},
-     [minutes, seconds, isRunning]);
-
-    /* Start */
-
-    const start = (e) => {
-        if (minutes !== 0 || seconds !== 0) {
-            setIsRunning(true);
-            setIsStopped(false);
-        } else {
-            window.alert("Insert time and sound");
-        }
+      }, 1000);
     }
 
-    /* Pause */
-
-    const pause = () => {
-        setIsRunning(false);
-        setIsStopped(true);
-    }
-    
-    /* Stop */
-
-
-    const stop = (e) => {
-        setIsRunning(false);
-        setSeconds(0);
-        setMinutes(0);
-        setIsStopped(true);
-    }
-    
-
-    /* time buttons */
-
-    const changeTimer = (e) => {
-        setIsRunning(false);
-        setSeconds(prevSeconds => {
-            prevSeconds = e.target.getAttribute("value")
-            return (
-                `${Math.floor(prevSeconds % 60) + "0"}`
-            )
-        });
-        setMinutes(prevMinutes => {
-            prevMinutes = e.target.getAttribute("value")
-            return (
-                `${Math.floor(prevMinutes / 60)}`
-            )
-        })
-       
+    if (minutes === 0 && seconds === 0) {
+      setIsRunning(false);
+      setSeconds(0);
+      setMinutes(0);
+      setIsStopped(true);
     }
 
+    return () => clearTimeout(interval);
+  }, [minutes, seconds, isRunning]);
 
-   
-    return (
-<>
+  /* Start */
 
-    <div className={style.timer}>
-            <div value={minutes} className={style.number}>{minutes}</div>
-            <div className={style.divider}>:</div>
-            <div value={seconds} className={style.number}>{seconds}</div>
-    </div>
-   
-    <div className={style.buttons}>
-               
-            <button value={300} className={style.buttonStyle} onClick={changeTimer}>
-             5 Minutes
-            </button>
-        
-          
-            <button value={600} className={style.buttonStyle} onClick={changeTimer} >
-             10 Minutes  
-            </button>
-          
-            <button value={900} className={style.buttonStyle} onClick={changeTimer} >
-              15 Minutes  
-            </button>
-   </div>
+  const start = (e) => {
+    if (minutes !== 0 || seconds !== 0) {
+      setIsRunning(true);
+      setIsStopped(false);
+    } else {
+      window.alert("Insert time and sound");
+    }
+  };
 
-        <div className={style.buttons}>
-              {!isRunning &&(
-                <button className={style.buttonStyle} onClick={start}  >
-                    Play
-                </button>
-              )} 
-               {isRunning && (
-                 <button className={style.buttonStyle} onClick={pause}>
-                    Pause 
-                 </button>
-                )}
-                <button className={style.buttonStyle} onClick={stop}>
-                    Stop 
-                </button>
+  /* Pause */
+
+  const pause = () => {
+    setIsRunning(false);
+    setIsStopped(true);
+  };
+
+  /* Stop */
+
+  const stop = (e) => {
+    setIsRunning(false);
+    setSeconds(0);
+    setMinutes(0);
+    setIsStopped(true);
+  };
+
+  /* time buttons */
+
+  const changeTimer = (e) => {
+    setIsRunning(false);
+    setSeconds((prevSeconds) => {
+      prevSeconds = e.target.getAttribute("value");
+      return `${Math.floor(prevSeconds % 60) + "0"}`;
+    });
+    setMinutes((prevMinutes) => {
+      prevMinutes = e.target.getAttribute("value");
+      return `${Math.floor(prevMinutes / 60)}`;
+    });
+  };
+
+  return (
+    <>
+      <div className={style.timer}>
+        <div value={minutes} className={style.number}>
+          {minutes}
         </div>
-         <p className={style.title}>
+        <div className={style.divider}>:</div>
+        <div value={seconds} className={style.number}>
+          {seconds}
+        </div>
+      </div>
 
-            <IoMdMusicalNote/>
-              Click on the photo to listen to relaxing background music
-            <IoMdMusicalNote/>
-            
-         </p>
-        
-            <div className={style.buttonSound}>
-            
-              <Sound/>
-            
-            </div>
-    
-</>   
-    )
+      <div className={style.buttons}>
+        <button value={300} className={style.buttonStyle} onClick={changeTimer}>
+          5 Minutes
+        </button>
 
+        <button value={600} className={style.buttonStyle} onClick={changeTimer}>
+          10 Minutes
+        </button>
+
+        <button value={900} className={style.buttonStyle} onClick={changeTimer}>
+          15 Minutes
+        </button>
+      </div>
+
+      <div className={style.buttons}>
+        {!isRunning && (
+          <button className={style.buttonStyle} onClick={start}>
+            Play
+          </button>
+        )}
+        {isRunning && (
+          <button className={style.buttonStyle} onClick={pause}>
+            Pause
+          </button>
+        )}
+        <button className={style.buttonStyle} onClick={stop}>
+          Stop
+        </button>
+      </div>
+      <p className={style.title}>
+        <IoMdMusicalNote />
+        Click on the photo to listen to relaxing background music
+        <IoMdMusicalNote />
+      </p>
+
+      <div className={style.buttonSound}>
+        <Sound />
+      </div>
+    </>
+  );
 }
