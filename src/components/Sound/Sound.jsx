f6f2bc8f286148e605effcf4737c53fbd2c7e979
Livia <livialuciani93@gmail.com>
2024-04-09 20:00:23 +0200

    mod

--- src/components/Sound/Sound.jsx
+++ src/components/Sound/Sound.jsx
@@ -1,16 +1,32 @@
 import style from "./Sound.module.css";
 import img from "../../assets/image/relax.jpg";
 import sound from "../../assets/sound/please-calm-my-mind.mp3";
+import { useEffect } from "react";
 
 export default function Sound() {
   let audio = new Audio(sound);
+  let isPlaying = false;
 
-  const start = () => {
-    audio.paused ? audio.play() : audio.pause();
+  const toggleAudio = () => {
+    if (isPlaying) {
+      audio.pause();
+      isPlaying = false;
+    } else {
+      audio.play();
+      isPlaying = true;
+    }
   };
 
+  useEffect(() => {
+    return () => {
+      if (audio) {
+        audio.pause();
+      }
+    };
+  }, []);
+
   return (
-    <button className={style.button} onClick={start}>
+    <button className={style.button} onClick={toggleAudio}>
       <img className={style.img} src={img} alt="image/soundpage" />
       <audio src={sound} />
     </button>
